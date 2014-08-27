384351cc01c9335c5413e16a5d9cb61aa7133759
indykpol <indykpol@gmail.com>
2014-08-27 17:11:16 +0200

    updated simulation code to use latest parallellization options provided in R 3.0

--- sim codes/simulating_data_mc_mvnorm_dilution.R
+++ sim codes/simulating_data_mc_mvnorm_dilution.R
@@ -1,4 +1,4 @@
-require(multicore)
+require(parallel)
 require(MASS)
 
 transform <- function(vector,targetMean,targetSd) {
@@ -30,103 +30,81 @@ sim.params <- expand.grid(dilution,read_counts,correlations_normals,correlations
 sim.params <- as.data.frame(sim.params)
 colnames(sim.params) <- c("dilution","read_counts","correlations_normals","correlations_tumours","difference_fc","error_terms")
 
-
-sim.data <- local({
-	f <- fifo(tempfile(), open="w+b", blocking=T)
-	if (inherits(fork(), "masterProcess")) {
-		# Child
-		progress <- 0.0
-		ptm <- proc.time()[3]
-		while (progress < 1 && !isIncomplete(f)) {
-			msg <- readBin(f, "double")
-			progress <- progress + as.numeric(msg)
-			if (round(100*progress,digits=2)%%0.25==0) {
-				eta <- ((proc.time()[3] - ptm) / progress) - (proc.time()[3] - ptm)
-				cat(sprintf("Progress: %.2f%% ETA: %.2fmin\n", progress * 100, eta/60))
-			}
-		} 
-		exit()
+numJobs <- nrow(sim.params)*1000
+sim.data <- mclapply(1:numJobs, mc.cores=47, mc.cleanup = TRUE, mc.preschedule = TRUE, function(i) {
+	current_param <- ceiling(i / 1000)
+	dilution <- sim.params[current_param,1]
+	read_count <- sim.params[current_param,2]
+	correlation_normals <- sim.params[current_param,3]
+	correlation_tumours <- sim.params[current_param,4]
+	diff <- sim.params[current_param,5]
+	error_term <- sim.params[current_param,6]
+	
+	Sigma <- matrix(c(3,1.5,-1.5,1.5,3,-3,-1.5,-3,3),3,3,byrow=TRUE)
+	temp <- mvrnorm(n=100, c(0,0,0), Sigma)
+	temp_counts <- temp[,1]
+	temp_gb <- temp[,2]
+	temp_gb <- transform(vector=temp_gb,targetMean=0,targetSd=2*error_term)
+	temp_pr <- temp[,3]
+	temp_pr <- transform(vector=temp_pr,targetMean=0,targetSd=2*error_term)
+	temp_counts <- round(transform(vector=temp_counts,targetMean=read_count,targetSd=error_term*read_count),digits=0)
+	temp_an <- cbind(temp_counts,temp_gb,temp_pr)
+	temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_an))
+	temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_an))
+	gb_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
+	pr_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
+	for (cpg in 1:11) {
+		temp_gb_cpg[,cpg] <- transform(vector=temp_an[,2],targetMean=gb_cpg_means[cpg],targetSd=1.5*0.14)
+		temp_pr_cpg[,cpg] <- transform(vector=temp_an[,3],targetMean=pr_cpg_means[cpg],targetSd=1.5*0.14)
 	}
-	numJobs <- nrow(sim.params)*1000
-	result <- mclapply(1:numJobs, mc.cores=47, mc.cleanup = TRUE, mc.preschedule = TRUE, function(i) {
-		current_param <- ceiling(i / 1000)
-		dilution <- sim.params[current_param,1]
-		read_count <- sim.params[current_param,2]
-		correlation_normals <- sim.params[current_param,3]
-		correlation_tumours <- sim.params[current_param,4]
-		diff <- sim.params[current_param,5]
-		error_term <- sim.params[current_param,6]
-		
+	temp_an <- cbind(temp_an,temp_gb_cpg,temp_pr_cpg)
+	colnames(temp_an) <- c("count","gb","pr",paste("gb",seq(1,11,1),sep=""),paste("pr",seq(1,11,1),sep=""))
+	
+	# True Ts
+	temp_t <- matrix(ncol=25,nrow=100,data=rep(0,25*100))
+	n = round(100*(1-dilution))
+	if (n > 0) {
+		Sigma <- matrix(c(3,1.5-3*correlation_normals,-1.5+3*correlation_normals,1.5-3*correlation_normals,3,-3,-1.5+3*correlation_normals,-3,3),3,3,byrow=TRUE)
+		temp <- mvrnorm(n=n, c(0,0,0), Sigma)
+		temp_counts <- temp[,1]
+		temp_gb <- temp[,2]
+		temp_gb <- transform(vector=temp_gb,targetMean=diff*2.5,targetSd=2*error_term)
+		temp_pr <- temp[,3]
+		temp_pr <- transform(vector=temp_pr,targetMean=-diff*2.5,targetSd=2*error_term)
+		temp_counts <- transform(vector=temp_counts,targetMean=read_count*(1+diff),targetSd=error_term*read_count*(1+diff))
+		temp_t[1:n,1:3] <- cbind(temp_counts,temp_gb,temp_pr) 
+		temp_t[1:n,1] <- round(temp_t[1:n,1],digits=0)
+		temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_t[1:n,]))
+		temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_t[1:n,]))
+		for (cpg in 1:11) {
+		temp_gb_cpg[,cpg] <- transform(vector=temp_t[1:n,2],targetMean=gb_cpg_means[cpg]+diff*2.5,targetSd=1.5*0.14)
+		temp_pr_cpg[,cpg] <- transform(vector=temp_t[1:n,3],targetMean=pr_cpg_means[cpg]-diff*2.5,targetSd=1.5*0.14)
+		}
+		temp_t[1:n,4:25] <- cbind(temp_gb_cpg,temp_pr_cpg)
+	}
+	# Fake Ts
+	n = 100-n
+	if (n > 0) {
 		Sigma <- matrix(c(3,1.5,-1.5,1.5,3,-3,-1.5,-3,3),3,3,byrow=TRUE)
-		temp <- mvrnorm(n=100, c(0,0,0), Sigma)
+		temp <- mvrnorm(n=n, c(0,0,0), Sigma)
 		temp_counts <- temp[,1]
 		temp_gb <- temp[,2]
 		temp_gb <- transform(vector=temp_gb,targetMean=0,targetSd=2*error_term)
 		temp_pr <- temp[,3]
 		temp_pr <- transform(vector=temp_pr,targetMean=0,targetSd=2*error_term)
 		temp_counts <- round(transform(vector=temp_counts,targetMean=read_count,targetSd=error_term*read_count),digits=0)
-		temp_an <- cbind(temp_counts,temp_gb,temp_pr)
-		temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_an))
-		temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_an))
+		temp_fake <- cbind(temp_counts,temp_gb,temp_pr)
+		temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_fake))
+		temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_fake))
 		gb_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
 		pr_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
 		for (cpg in 1:11) {
-			temp_gb_cpg[,cpg] <- transform(vector=temp_an[,2],targetMean=gb_cpg_means[cpg],targetSd=1.5*0.14)
-			temp_pr_cpg[,cpg] <- transform(vector=temp_an[,3],targetMean=pr_cpg_means[cpg],targetSd=1.5*0.14)
+		temp_gb_cpg[,cpg] <- transform(vector=temp_fake[,2],targetMean=gb_cpg_means[cpg],targetSd=1.5*0.14)
+		temp_pr_cpg[,cpg] <- transform(vector=temp_fake[,3],targetMean=pr_cpg_means[cpg],targetSd=1.5*0.14)
 		}
-		temp_an <- cbind(temp_an,temp_gb_cpg,temp_pr_cpg)
-		colnames(temp_an) <- c("count","gb","pr",paste("gb",seq(1,11,1),sep=""),paste("pr",seq(1,11,1),sep=""))
-		
-		# True Ts
-		temp_t <- matrix(ncol=25,nrow=100,data=rep(0,25*100))
-		n = round(100*(1-dilution))
-		if (n > 0) {
-			Sigma <- matrix(c(3,1.5-3*correlation_normals,-1.5+3*correlation_normals,1.5-3*correlation_normals,3,-3,-1.5+3*correlation_normals,-3,3),3,3,byrow=TRUE)
-			temp <- mvrnorm(n=n, c(0,0,0), Sigma)
-			temp_counts <- temp[,1]
-			temp_gb <- temp[,2]
-			temp_gb <- transform(vector=temp_gb,targetMean=diff*2.5,targetSd=2*error_term)
-			temp_pr <- temp[,3]
-			temp_pr <- transform(vector=temp_pr,targetMean=-diff*2.5,targetSd=2*error_term)
-			temp_counts <- transform(vector=temp_counts,targetMean=read_count*(1+diff),targetSd=error_term*read_count*(1+diff))
-			temp_t[1:n,1:3] <- cbind(temp_counts,temp_gb,temp_pr) 
-			temp_t[1:n,1] <- round(temp_t[1:n,1],digits=0)
-			temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_t[1:n,]))
-			temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_t[1:n,]))
-			for (cpg in 1:11) {
-				temp_gb_cpg[,cpg] <- transform(vector=temp_t[1:n,2],targetMean=gb_cpg_means[cpg]+diff*2.5,targetSd=1.5*0.14)
-				temp_pr_cpg[,cpg] <- transform(vector=temp_t[1:n,3],targetMean=pr_cpg_means[cpg]-diff*2.5,targetSd=1.5*0.14)
-			}
-			temp_t[1:n,4:25] <- cbind(temp_gb_cpg,temp_pr_cpg)
-		}
-		# Fake Ts
-		n = 100-n
-		if (n > 0) {
-			Sigma <- matrix(c(3,1.5,-1.5,1.5,3,-3,-1.5,-3,3),3,3,byrow=TRUE)
-			temp <- mvrnorm(n=n, c(0,0,0), Sigma)
-			temp_counts <- temp[,1]
-			temp_gb <- temp[,2]
-			temp_gb <- transform(vector=temp_gb,targetMean=0,targetSd=2*error_term)
-			temp_pr <- temp[,3]
-			temp_pr <- transform(vector=temp_pr,targetMean=0,targetSd=2*error_term)
-			temp_counts <- round(transform(vector=temp_counts,targetMean=read_count,targetSd=error_term*read_count),digits=0)
-			temp_fake <- cbind(temp_counts,temp_gb,temp_pr)
-			temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_fake))
-			temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_fake))
-			gb_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
-			pr_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
-			for (cpg in 1:11) {
-				temp_gb_cpg[,cpg] <- transform(vector=temp_fake[,2],targetMean=gb_cpg_means[cpg],targetSd=1.5*0.14)
-				temp_pr_cpg[,cpg] <- transform(vector=temp_fake[,3],targetMean=pr_cpg_means[cpg],targetSd=1.5*0.14)
-			}
-			temp_fake <- cbind(temp_fake,temp_gb_cpg,temp_pr_cpg)
-			temp_t[(100-n+1):100,] <- temp_fake
-		}
-		colnames(temp_t) <- c("count","gb","pr",paste("gb",seq(1,11,1),sep=""),paste("pr",seq(1,11,1),sep=""))
-		writeBin(1/numJobs, f)
-		list(temp_an,temp_t)
-		})
-	close(f)
-	cat("Done\n")
-	result
-})
+		temp_fake <- cbind(temp_fake,temp_gb_cpg,temp_pr_cpg)
+		temp_t[(100-n+1):100,] <- temp_fake
+	}
+	colnames(temp_t) <- c("count","gb","pr",paste("gb",seq(1,11,1),sep=""),paste("pr",seq(1,11,1),sep=""))
+	list(temp_an,temp_t)
+	})
--- sim codes/simulating_data_mc_mvnorm_neg.R
+++ sim codes/simulating_data_mc_mvnorm_neg.R
@@ -1,4 +1,4 @@
-require(multicore)
+require(parallel)
 require(MASS)
 
 transform <- function(vector,targetMean,targetSd) {
@@ -29,78 +29,56 @@ sim.params <- cbind(sim.params,correlations_normals)
 sim.params <- sim.params[,c(1,2,5,3,4)]
 sim.params <- as.data.frame(sim.params)
 colnames(sim.params) <- c("read_counts","correlations_normals","correlations_tumours","difference_fc","error_terms")
-
-sim.data <- local({
-    f <- fifo(tempfile(), open="w+b", blocking=T)
-    if (inherits(fork(), "masterProcess")) {
-        # Child
-        progress <- 0.0
-		ptm <- proc.time()[3]
-        while (progress < 1 && !isIncomplete(f)) {
-            msg <- readBin(f, "double")
-            progress <- progress + as.numeric(msg)
-			if (round(100*progress,digits=2)%%0.25==0) {
-				eta <- ((proc.time()[3] - ptm) / progress) - (proc.time()[3] - ptm)
-				cat(sprintf("Progress: %.2f%% ETA: %.2fmin\n", progress * 100, eta/60))
-			}
-        } 
-        exit()
-    }
-    numJobs <- nrow(sim.params)*1000
-	result <- mclapply(1:numJobs, mc.cores=47, mc.cleanup = TRUE, mc.preschedule = TRUE, function(i) {
-		current_param <- ceiling(i / 1000)
-		read_count <- sim.params[current_param,1]
-		correlation_normals <- sim.params[current_param,2]
-		correlation_tumours <- sim.params[current_param,3]
-		diff <- sim.params[current_param,4]
-		error_term <- sim.params[current_param,5]
-		
-		Sigma <- matrix(c(3,1.5-3*correlation_normals,-1.5+3*correlation_normals,1.5-3*correlation_normals,3,-3,-1.5+3*correlation_normals,-3,3),3,3,byrow=TRUE)
-		temp <- mvrnorm(n=100, c(0,0,0), Sigma)
-		temp_counts <- temp[,1]
-		temp_gb <- temp[,2]
-		temp_gb <- transform(vector=temp_gb,targetMean=0,targetSd=2*error_term)
-		temp_pr <- temp[,3]
-		temp_pr <- transform(vector=temp_pr,targetMean=0,targetSd=2*error_term)
-		temp_counts <- round(transform(vector=temp_counts,targetMean=read_count,targetSd=error_term*read_count),digits=0)
-		temp_an <- cbind(temp_counts,temp_gb,temp_pr)
-		temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_an))
-		temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_an))
-		gb_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
-		pr_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
-		for (cpg in 1:11) {
-			temp_gb_cpg[,cpg] <- transform(vector=temp_an[,2],targetMean=gb_cpg_means[cpg],targetSd=1.5*0.14)
-			temp_pr_cpg[,cpg] <- transform(vector=temp_an[,3],targetMean=pr_cpg_means[cpg],targetSd=1.5*0.14)
-		}
-		temp_an <- cbind(temp_an,temp_gb_cpg,temp_pr_cpg)
-		colnames(temp_an) <- c("count","gb","pr",paste("gb",seq(1,11,1),sep=""),paste("pr",seq(1,11,1),sep=""))
-		
-		# True Ts
-		n = 100 #round(100*(1-bias))
-		Sigma <- matrix(c(3,1.5-3*correlation_normals,-1.5+3*correlation_normals,1.5-3*correlation_normals,3,-3,-1.5+3*correlation_normals,-3,3),3,3,byrow=TRUE)
-		temp <- mvrnorm(n=n, c(0,0,0), Sigma)
-		temp_counts <- temp[,1]
-		temp_gb <- temp[,2]
-		temp_gb <- transform(vector=temp_gb,targetMean=0,targetSd=2*error_term)
-		temp_pr <- temp[,3]
-		temp_pr <- transform(vector=temp_pr,targetMean=0,targetSd=2*error_term)
-		temp_counts <- transform(vector=temp_counts,targetMean=read_count,targetSd=error_term*read_count)
-		temp_t <- cbind(temp_counts,temp_gb,temp_pr)#*matrix(rnorm(round(300*(1-bias)),mean=1,sd=error),ncol=3)
-		temp_t[,1] <- round(temp_t[,1],digits=0)
-		temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_t))
-		temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_t))
-		#gb_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
-		#pr_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
-		for (cpg in 1:11) {
-			temp_gb_cpg[,cpg] <- transform(vector=temp_t[,2],targetMean=gb_cpg_means[cpg],targetSd=1.5*0.14)
-			temp_pr_cpg[,cpg] <- transform(vector=temp_t[,3],targetMean=pr_cpg_means[cpg],targetSd=1.5*0.14)
-		}
-		temp_t <- cbind(temp_t,temp_gb_cpg,temp_pr_cpg)
-		colnames(temp_t) <- c("count","gb","pr",paste("gb",seq(1,11,1),sep=""),paste("pr",seq(1,11,1),sep=""))
-		writeBin(1/numJobs, f)
-		list(temp_an,temp_t)
-		})
-	close(f)
-	cat("Done\n")
-	result
-})
+numJobs <- nrow(sim.params)*1000
+sim.data <- mclapply(1:numJobs, mc.cores=47, mc.cleanup = TRUE, mc.preschedule = TRUE, function(i) {
+	current_param <- ceiling(i / 1000)
+	read_count <- sim.params[current_param,1]
+	correlation_normals <- sim.params[current_param,2]
+	correlation_tumours <- sim.params[current_param,3]
+	diff <- sim.params[current_param,4]
+	error_term <- sim.params[current_param,5]
+	
+	Sigma <- matrix(c(3,1.5-3*correlation_normals,-1.5+3*correlation_normals,1.5-3*correlation_normals,3,-3,-1.5+3*correlation_normals,-3,3),3,3,byrow=TRUE)
+	temp <- mvrnorm(n=100, c(0,0,0), Sigma)
+	temp_counts <- temp[,1]
+	temp_gb <- temp[,2]
+	temp_gb <- transform(vector=temp_gb,targetMean=0,targetSd=2*error_term)
+	temp_pr <- temp[,3]
+	temp_pr <- transform(vector=temp_pr,targetMean=0,targetSd=2*error_term)
+	temp_counts <- round(transform(vector=temp_counts,targetMean=read_count,targetSd=error_term*read_count),digits=0)
+	temp_an <- cbind(temp_counts,temp_gb,temp_pr)
+	temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_an))
+	temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_an))
+	gb_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
+	pr_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
+	for (cpg in 1:11) {
+		temp_gb_cpg[,cpg] <- transform(vector=temp_an[,2],targetMean=gb_cpg_means[cpg],targetSd=1.5*0.14)
+		temp_pr_cpg[,cpg] <- transform(vector=temp_an[,3],targetMean=pr_cpg_means[cpg],targetSd=1.5*0.14)
+	}
+	temp_an <- cbind(temp_an,temp_gb_cpg,temp_pr_cpg)
+	colnames(temp_an) <- c("count","gb","pr",paste("gb",seq(1,11,1),sep=""),paste("pr",seq(1,11,1),sep=""))
+	
+	# True Ts
+	n = 100 #round(100*(1-bias))
+	Sigma <- matrix(c(3,1.5-3*correlation_normals,-1.5+3*correlation_normals,1.5-3*correlation_normals,3,-3,-1.5+3*correlation_normals,-3,3),3,3,byrow=TRUE)
+	temp <- mvrnorm(n=n, c(0,0,0), Sigma)
+	temp_counts <- temp[,1]
+	temp_gb <- temp[,2]
+	temp_gb <- transform(vector=temp_gb,targetMean=0,targetSd=2*error_term)
+	temp_pr <- temp[,3]
+	temp_pr <- transform(vector=temp_pr,targetMean=0,targetSd=2*error_term)
+	temp_counts <- transform(vector=temp_counts,targetMean=read_count,targetSd=error_term*read_count)
+	temp_t <- cbind(temp_counts,temp_gb,temp_pr)#*matrix(rnorm(round(300*(1-bias)),mean=1,sd=error),ncol=3)
+	temp_t[,1] <- round(temp_t[,1],digits=0)
+	temp_pr_cpg <- matrix(ncol=11,nrow=nrow(temp_t))
+	temp_gb_cpg <- matrix(ncol=11,nrow=nrow(temp_t))
+	#gb_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
+	#pr_cpg_means <- findMean(n=11,mean=0,sd=1.5*0.14)
+	for (cpg in 1:11) {
+		temp_gb_cpg[,cpg] <- transform(vector=temp_t[,2],targetMean=gb_cpg_means[cpg],targetSd=1.5*0.14)
+		temp_pr_cpg[,cpg] <- transform(vector=temp_t[,3],targetMean=pr_cpg_means[cpg],targetSd=1.5*0.14)
+	}
+	temp_t <- cbind(temp_t,temp_gb_cpg,temp_pr_cpg)
+	colnames(temp_t) <- c("count","gb","pr",paste("gb",seq(1,11,1),sep=""),paste("pr",seq(1,11,1),sep=""))
+	list(temp_an,temp_t)
+	})
