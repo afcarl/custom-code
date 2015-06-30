f2adcf121f4f27590b7170e94369d27704a95538
indykpol <indykpol@gmail.com>
2015-06-30 12:02:49 +0200

    Updates and addition

--- exporting selected genes in a list.R
+++ exporting selected genes in a list.R
@@ -1,14 +1,31 @@
 data_BRCA <- list()
-for (i in 1:10) {
+for (i in 1:17728) {
 	IDs_promoter <- unique(c(eval(parse(text = paste('TSS1500Ind$SID$','"',workingList_BRCA[i],'"',sep=""))),eval(parse(text = paste('TSS200Ind$SID$','"',workingList_BRCA[i],'"',sep=""))), eval(parse(text = paste('UTR5Ind$SID$','"',workingList_BRCA[i],'"',sep="")))),eval(parse(text = paste('EXON1Ind$SID$','"',workingList_BRCA[i],'"',sep=""))))
 	IDs_body <- unique(c(eval(parse(text = paste('GENEBODYInd$SID$','"',workingList_BRCA[i],'"',sep=""))), eval(parse(text = paste('UTR3Ind$SID$','"',workingList_BRCA[i],'"',sep="")))))
 	ncol = length(IDs_promoter) + length(IDs_body) + 2
-	temp <- matrix(nrow=150,ncol=ncol)
-	temp[,1] <- factors_ls[c(ANs,Ts)]*10^6
-	temp[,2] <- counts_BRCA_plusOne[workingList_BRCA[i],c(ANs,Ts)]
-	temp[,3:(2+length(IDs_promoter))] <- t(mmatrix[IDs_promoter,c(ANs,Ts)])
-	temp[,(3+length(IDs_promoter)):(2+length(IDs_promoter)+length(IDs_body))] <- t(mmatrix[IDs_body,c(ANs,Ts)])
+	nrow = length(c(G1,G2))
+	temp <- matrix(nrow=nrow,ncol=ncol)
+	temp[,1] <- unlist(factors_ls[c(G1,G2)])*10^6
+	temp[,2] <- unlist(counts[workingList_BRCA[i],c(G1,G2)])
+	temp[,3:(2+length(IDs_promoter))] <- t(mmatrix_pc[IDs_promoter,c(G1,G2)])
+	temp[,(3+length(IDs_promoter)):(2+length(IDs_promoter)+length(IDs_body))] <- t(mmatrix_pc[IDs_body,c(G1,G2)])
 	colnames(temp) <- c("lib_size","read_count",paste("pr_",1:length(IDs_promoter),sep=""),paste("gb_",1:length(IDs_body),sep=""))
-	rownames(temp) <- c(ANs,Ts)
+	rownames(temp) <- c(G1,G2)
 	data_BRCA[[workingList_BRCA[i]]] <- temp
+}
+
+data_BRCA_progressing <- list()
+for (i in 1:17728) {
+	IDs_promoter <- unique(c(eval(parse(text = paste('TSS1500Ind$SID$','"',workingList_BRCA[i],'"',sep=""))),eval(parse(text = paste('TSS200Ind$SID$','"',workingList_BRCA[i],'"',sep=""))), eval(parse(text = paste('UTR5Ind$SID$','"',workingList_BRCA[i],'"',sep="")))),eval(parse(text = paste('EXON1Ind$SID$','"',workingList_BRCA[i],'"',sep=""))))
+	IDs_body <- unique(c(eval(parse(text = paste('GENEBODYInd$SID$','"',workingList_BRCA[i],'"',sep=""))), eval(parse(text = paste('UTR3Ind$SID$','"',workingList_BRCA[i],'"',sep="")))))
+	ncol = length(IDs_promoter) + length(IDs_body) + 2
+	nrow = length(c(progressed,non_progressed))
+	temp <- matrix(nrow=nrow,ncol=ncol)
+	temp[,1] <- unlist(factors_ls[c(progressed,non_progressed)])*10^6
+	temp[,2] <- unlist(counts[workingList_BRCA[i],c(progressed,non_progressed)])
+	temp[,3:(2+length(IDs_promoter))] <- t(mmatrix_pc[IDs_promoter,c(progressed,non_progressed)])
+	temp[,(3+length(IDs_promoter)):(2+length(IDs_promoter)+length(IDs_body))] <- t(mmatrix_pc[IDs_body,c(progressed,non_progressed)])
+	colnames(temp) <- c("lib_size","read_count",paste("pr_",1:length(IDs_promoter),sep=""),paste("gb_",1:length(IDs_body),sep=""))
+	rownames(temp) <- c(progressed,non_progressed)
+	data_BRCA_progressing[[workingList_BRCA[i]]] <- temp
 }
--- missing.R
+++ missing.R
@@ -1,4 +1,4 @@
 found <- read.table("findings.txt",header=FALSE)
 found <- found[,1]
-all <- c(3001:4000,11001:12000
+all <- 1:17728
 all[-which(all %in% found)]
